fc2411e5a7777da17a4bfdb0381403face29942c
Hob3rt <1215315078@qq.com>
2025-01-03 18:58:52 +0800

    日志
    通过将外部依赖库作为子模块添加到主仓库中，可以保持主仓库和子模块仓库的独立性。
    
    内联函数inline：
    1.使用 inline 修饰避免了频繁调用函数对栈内存重复开辟所带来的消耗
    2.inline 只适合涵数体内代码简单的涵数使用，不能包含复杂的结构控制语句例如 while、switch，并且不能内联函数本身不能是直接递归函数
    3.inline 函数仅仅是一个对编译器的建议，所以最后能否真正内联，看编译器的意思，它如果认为函数不复杂，能在调用点展开，就会真正内联，并不是说声明了内联就会内联，声明内联只是一个建议而已
    4.每个调用了内联函数的文件都出现了该内联函数的定义。因此，将内联函数的定义放在头文件里实现是合适的
    5.定义在类中的成员函数默认都是内联的

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -3,6 +3,12 @@
 extern Hazel::Application* Hazel::CreateApplication();
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 11;
+	HZ_INFO("hello Var = {0}",a);
+
+
 	printf("Welcome to Hazel!");
 	auto app = Hazel::CreateApplication();
 	app->Run();
