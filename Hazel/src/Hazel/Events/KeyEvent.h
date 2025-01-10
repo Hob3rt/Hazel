2a8c3a7c284e8bac9b6ad0d3ff9bc39ed79d505e
Hob3rt <1215315078@qq.com>
2025-01-10 13:12:41 +0800

    事件系统

---
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Event.h"
+
+#include <sstream>
+
+namespace Hazel
+{
+	class HAZEL_API KeyEvent :public Event //抽象类无法实例一个KeyEvent
+	{
+	public:
+		inline int GetKeyCode()const { return m_KeyCode; };
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard |EventCategoryInput)
+
+	protected:
+		KeyEvent(int keycode):m_KeyCode(keycode){}//protected类型构造 没有别的手段构造KeyEvent
+		int m_KeyCode;
+	};
+
+	class HAZEL_API KeyPressedEvent : public KeyEvent
+	{
+	public:
+		KeyPressedEvent(int keycode, int repeatCount): KeyEvent(keycode),m_RepeatCount(repeatCount){}
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << "(" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+		EVENT_CLASS_TYPE(KeyPressed)
+	private:
+		int m_RepeatCount;
+	};
+}
--- premake5.lua
+++ premake5.lua
@@ -75,7 +75,8 @@ project "Hazel"
 
 	includedirs
 	{
-		"%{prj.name}/vendor/spdlog/include"
+		"%{prj.name}/vendor/spdlog/include",
+		"%{prj.name}/src"
 	}
 
 
