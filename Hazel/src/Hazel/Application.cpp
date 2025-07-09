fba506d3552b6d1c4b9bf967fb8a3c86a50ec4f9
Hob3rt <1215315078@qq.com>
2025-07-09 19:41:45 +0800

    Input轮询
    cpp17中有 auto[x,y] = getPosition()的用法,后面的函数返回std::pair

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -4,6 +4,7 @@
 
 #include <glad/glad.h>
 
+#include "Hazel/Input.h"
 namespace Hazel
 {
 	#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -58,7 +59,8 @@ namespace Hazel
 				for (Layer* layer : m_LayerStack)
 					layer->OnUpdate();
 
-
+				auto [x, y] = Input::GetMousePosition();
+				HZ_CORE_TRACE("{0},{1}",x,y);
 				m_Window->OnUpdate();
 			}
 
